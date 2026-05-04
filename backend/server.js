4772105a2fc4ef3b90c85b221c760cc591c6a339
Vikita <kvikitareddy@gmail.com>
2026-05-04 11:16:46 +0530

    fix route error

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.get("/api/test", authMiddleware, (req, res) => {
 app.use(express.static(path.join(__dirname, "../my-app/build")));
 
 // Catch-all route (IMPORTANT)
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../my-app/build", "index.html"));
 });
 
