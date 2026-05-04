e556f716ae07e33f1a0d86643b899cf47d2ef7fa
Vikita <kvikitareddy@gmail.com>
2026-05-04 11:57:46 +0530

    fix api url

--- my-app/src/services/api.js
+++ my-app/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+const api = axios.create({
+  baseURL: "https://team-task-manager-amhb.onrender.com/api"
 });
 
-export default API;
+export default api;
